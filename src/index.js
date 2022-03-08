26d628273162d17461aabf84dccd894e12f762ee
Adam Smith <91632531+Roadlyfe@users.noreply.github.com>
2022-03-08 16:14:28 -0600

    through eventListener 6

--- src/index.js
+++ src/index.js
@@ -6,21 +6,38 @@ console.log('hello world');
 
 //load event
 window.onload = function (evt) {
-    console.log(`event ${evt.type} fired! Ready to go!`)
-const heading = document.querySelector("h1")
-heading.textContent = "Maybe not so fun after all. "
+        console.log(`event ${evt.type} fired! Ready to go!`)
+    const heading = document.querySelector("h1")
+    heading.textContent = "Maybe not so fun after all. "
 
 
-//copy
-window.addEventListener('copy', () => {
-    navigator.clipboard.readText()
-        .then(text => {
-        heading.textContent += text
+    //copy
+    window.addEventListener('copy', () => {
+        navigator.clipboard.readText()
+            .then(text => {
+            heading.textContent += text
+            })
         })
+    //click
+        document.body.addEventListener('click', evt => {
+            evt.target.classList.toggle('mirror')
+        })
+    //dblclick
+    document.body.addEventListener('dblclick', evt => {
+        evt.target.innerHTML = '';
+    })
+    
+    //keydown 
+    window.addEventListener('keydown', evt => {
+       if(evt.key == 6) {
+           document.body.innerHTML = '<h1>You destroyed this page!</h1>';
+       }
     })
 
-    document.body.addEventListener('click', evt => {
-        evt.target.classList.toggle('mirror')
+    //mousemove
+    document.body.addEventListener('mousemove', evt => {
+        const { clientX, clientY} = evt;
+        console.log(`mouse is at ${clientX}, ${clientY}`);
     })
 }
 
