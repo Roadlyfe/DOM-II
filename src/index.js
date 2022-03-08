2bc2bbe44a55a7fcb557e359a04989a1c76d45a4
Adam Smith <91632531+Roadlyfe@users.noreply.github.com>
2022-03-08 15:48:24 -0600

    third event listener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import './less/index.less'
 
 // Your code goes here!
+console.log('hello world');
+
+
+//load event
+window.onload = function (evt) {
+    console.log(`event ${evt.type} fired! Ready to go!`)
+const heading = document.querySelector("h1")
+heading.textContent = "Maybe not so fun after all. "
+
+
+//copy
+window.addEventListener('copy', () => {
+    navigator.clipboard.readText()
+        .then(text => {
+        heading.textContent += text
+        })
+    })
+
+    document.body.addEventListener('click', evt => {
+        evt.target.classList.toggle('mirror')
+    })
+}
+
+
+
